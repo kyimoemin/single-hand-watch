14e6f66aeac299244192c54e454639cb97020756
kyimoemin <kyimoemin0@gmail.com>
2025-03-22 03:01:31 +0700

    make hours follow hand

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const hand = document.querySelector(".hand");
-const hours = document.querySelector(".hours");
-
+const hoursElement = document.querySelector(".hours");
 function rotate() {
-  if (!hand || !hours) return;
   const date = new Date();
   const minutes = date.getMinutes() + date.getSeconds() / 60;
+  const hours = date.getHours() + minutes / 60;
   hand.style.transform = `rotate(${minutes * 6}deg)`;
+  hoursElement.style.transform = `rotate(${30 * (6 - hours) + minutes * 6}deg)`;
 }
 setInterval(() => {
   rotate();
