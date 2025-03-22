17b422188d82bbd4ac8e94eb0f71eeadcc48c593
kyimoemin <kyimoemin0@gmail.com>
2025-03-22 16:00:14 +0700

    added correct minute hand shadow

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+/**
+ * @type {HTMLSpanElement|null}
+ */
 const hand = document.querySelector(".hand");
+/**
+ * @type {HTMLDivElement|null}
+ */
 const hoursElement = document.querySelector(".hours");
+
 function rotate() {
+  if (!hand || !hoursElement) throw new Error("Element not found");
   const date = new Date();
   const minutes = date.getMinutes() + date.getSeconds() / 60;
   const hours = date.getHours() + minutes / 60;
-  hand.style.transform = `rotate(${minutes * 6}deg)`;
-  hoursElement.style.transform = `rotate(${30 * (6 - hours) + minutes * 6}deg)`;
+  const minuteDegree = minutes * 6;
+  const hoursDegree = 30 * (6 - hours) + minutes * 6;
+
+  hand.style.transform = `rotate(${minuteDegree}deg)`;
+  hoursElement.style.transform = `rotate(${hoursDegree}deg)`;
+  document.documentElement.style.setProperty(
+    "--hand-shadow",
+    getHandShadow(minuteDegree)
+  );
 }
+
+function getHandShadow(degree) {
+  degree += 45; // 45 degree of shadow
+  const radians = degree * (Math.PI / 180);
+  const x = 5 * Math.sin(radians);
+  const y = 5 * Math.cos(radians);
+  return `${x}px ${y}px 5px -1px  hsla(0, 0%, 0%, 1)`;
+}
+
 setInterval(() => {
   rotate();
 }, 1000);
