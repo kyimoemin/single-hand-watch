dc0f52805102120a81e0c3f7e1637165e93571fb
kyimoemin <kyimoemin0@gmail.com>
2025-03-22 20:52:51 +0700

    add light shadow

--- watch_1/script.js
+++ watch_1/script.js
@@ -28,7 +28,10 @@ function getHandShadow(degree) {
   const radians = degree * (Math.PI / 180);
   const x = 5 * Math.sin(radians);
   const y = 5 * Math.cos(radians);
-  return `${x}px ${y}px 5px -1px  hsla(0, 0%, 0%, 1)`;
+  const lx = 1 * Math.sin(radians);
+  const ly = 1 * Math.cos(radians);
+  console.log({ lx, ly });
+  return `${lx}px ${ly}px 1px -0.5px hsla(0,0%,100%,1) inset ,${x}px ${y}px 5px -1px  hsla(0, 0%, 0%, 1)`;
 }
 
 setInterval(() => {
